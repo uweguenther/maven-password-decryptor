d31872f1d2833b902df4e0423cc3d8a77cd9e204
Uwe Günther <uwe@cscc.de>
2017-09-09 16:07:13 +0200

    Initial commit and push

---
+++ maven-password-decryptor/src/main/java/de/cscc/maven/MavenPasswordDecryptor.java
@@ -0,0 +1,23 @@
+package de.cscc.maven;
+
+import org.sonatype.plexus.components.cipher.DefaultPlexusCipher;
+
+public class MavenPasswordDecryptor {
+	public static void main(String[] args) throws Exception {
+		
+		if (args.length < 1 || args.length > 2 ) {
+			System.out.println("Usage: java -jar maven-password-decryptor.jar <encrypted-password>");
+			System.out.println("Usage: java -jar maven-password-decryptor.jar <encrypted-passworrd> <master-password>");
+			return;
+		}
+		
+		DefaultPlexusCipher cipher = new DefaultPlexusCipher();
+
+		String encryptedPassword = args[0];
+		String passPhrase = (args.length == 2 && args[1] != null && !args[1].isEmpty()) ? args[1] : "settings.security";
+		
+		String result = cipher.decryptDecorated(encryptedPassword, passPhrase);
+
+		System.out.println(result);
+	}
+}
